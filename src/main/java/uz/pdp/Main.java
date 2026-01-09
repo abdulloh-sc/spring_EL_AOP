2f41988f03feb2369de057045c8cb8de43b55f3d
abdulloh7111 <abdullohjon101@gmail.com>
2026-01-09 21:14:19 +0500

    AOP_xml

--- src/main/java/uz/pdp/Main.java
+++ src/main/java/uz/pdp/Main.java
@@ -1,7 +1,16 @@
 package uz.pdp;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
 
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ioc_settings.xml");
+        Performance performance = context.getBean(Performance.class);
+        try {
+            performance.perform();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
